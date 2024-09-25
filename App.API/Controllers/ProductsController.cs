246c843451ce082b1ae77915abdb9a8d7fba169d
Serhat Tomak <serhattomak@gmail.com>
2024-09-25 14:15:38 +0300

    Pagination Endpoint
    Created an endpoint which use pageNumber and pageSize for showing the datas from the database with pagination.

--- App.API/Controllers/ProductsController.cs
+++ App.API/Controllers/ProductsController.cs
@@ -6,7 +6,9 @@ namespace App.API.Controllers
 	public class ProductsController(IProductService productService) : CustomBaseController
 	{
 		[HttpGet]
-		public async Task<IActionResult> GetAll()=> CreateActionResult(await productService.GetAllListAsync());
+		public async Task<IActionResult> GetAll() => CreateActionResult(await productService.GetAllListAsync());
+		[HttpGet("{pageNumber}/{pageSize}")]
+		public async Task<IActionResult> GetPagedAll(int pageNumber, int pageSize) => CreateActionResult(await productService.GetPagedAllListAsync(pageNumber, pageSize));
 		[HttpGet("{id}")]
 		public async Task<IActionResult> GetById(int id) => CreateActionResult(await productService.GetByIdAsync(id));
 		[HttpPost]
--- Services/Products/ProductService.cs
+++ Services/Products/ProductService.cs
@@ -25,6 +25,14 @@ public class ProductService(IProductRepository productRepository, IUnitOfWork un
 
 		return ServiceResult<List<ProductDto>>.Success(productsAsDto);
 	}
+
+	public async Task<ServiceResult<List<ProductDto>>> GetPagedAllListAsync(int pageNumber, int pageSize)
+	{
+		var products = await productRepository.GetAll().Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+		var productsAsDto = products.Select(p => new ProductDto(p.Id, p.Name, p.Price, p.Stock)).ToList();
+
+		return ServiceResult<List<ProductDto>>.Success(productsAsDto);
+	}
 	public async Task<ServiceResult<ProductDto?>> GetByIdAsync(int id)
 	{
 		var product = await productRepository.GetByIdAsync(id);
