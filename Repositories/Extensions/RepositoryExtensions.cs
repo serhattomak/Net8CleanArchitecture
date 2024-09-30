fa4bca7db28fe9425efec34dec93ab1ee12f6bd4
Serhat Tomak <serhattomak@gmail.com>
2024-09-30 17:16:18 +0300

    SaveChanges Interceptor
    Created SaveChangesInterceptor for tracking created and updated dates and implemented in the codes.

--- Repositories/Extensions/RepositoryExtensions.cs
+++ Repositories/Extensions/RepositoryExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using App.Repositories.Categories;
+using App.Repositories.Interceptors;
 using App.Repositories.Products;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,8 @@ public static class RepositoryExtensions
 			{
 				sqlServerOptionsAction.MigrationsAssembly(typeof(RepositoryAssembly).Assembly.FullName);
 			});
+
+			options.AddInterceptors(new AuditDbContextInterceptor());
 		});
 		services.AddScoped<IProductRepository, ProductRepository>();
 		services.AddScoped<ICategoryRepository, CategoryRepository>();
---
+++ Repositories/Interceptors/AuditDbContextInterceptor.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace App.Repositories.Interceptors;
+
+public class AuditDbContextInterceptor : SaveChangesInterceptor
+{
+	private static readonly Dictionary<EntityState, Action<DbContext, IAuditEntity>> Behaviours = new()
+	{
+		{EntityState.Added, AddBehaviour},
+		{EntityState.Modified, ModifiedBehaviour}
+	};
+	private static void AddBehaviour(DbContext context, IAuditEntity auditEntity)
+	{
+		auditEntity.Created = DateTime.UtcNow;
+		context.Entry(auditEntity).Property(x => x.Updated).IsModified = false;
+	}
+
+	private static void ModifiedBehaviour(DbContext context, IAuditEntity auditEntity)
+	{
+		context.Entry(auditEntity).Property(x => x.Created).IsModified = false;
+		auditEntity.Updated = DateTime.UtcNow;
+	}
+
+	public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result,
+		CancellationToken cancellationToken = new CancellationToken())
+	{
+		foreach (var entityEntry in eventData.Context!.ChangeTracker.Entries().ToList())
+		{
+			if (entityEntry.Entity is not IAuditEntity auditEntity) continue;
+
+			Behaviours[entityEntry.State](eventData.Context, auditEntity);
+
+			#region 1st way
+
+			//switch (entityEntry.State)
+			//{
+			//	case EntityState.Added:
+			//		AddBehaviour(eventData.Context, auditEntity);
+			//		break;
+
+			//	case EntityState.Modified:
+			//		ModifiedBehaviour(eventData.Context, auditEntity);
+			//		break;
+			//}
+
+			#endregion
+		}
+
+		return base.SavingChangesAsync(eventData, result, cancellationToken);
+	}
+}
